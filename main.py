0c7a4fb65077da677f32d1393c64bc2b85f06211
Vladimir <termvsrobo@gmail.com>
2022-06-25 13:53:31 +0700

    init commit
    test sentry

---
+++ main.py
@@ -0,0 +1,11 @@
+import sentry_sdk
+sentry_sdk.init(
+    dsn="https://7ae8944ef0ce4192993136fc95051e9a@o1298570.ingest.sentry.io/6529702",
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0
+)
+
+division_by_zero = 1 / 0
